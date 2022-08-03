55c8a22fa7f11ee6f6becd8941d2d5a92763864d
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-08-03 16:54:43 +0100

    second commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom"
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home")
   return (
-    <nav className='font-bold h-[96px] w-full  pl-20 pr-20 flex justify-center items-center'>
+    <nav className='bg-white font-bold h-[96px] w-full sticky top-0 pl-20 pr-20 flex justify-center items-center shadow-lg'>
         <div className='w-[86%] flex justify-between items-center'>
             <Link to="/" >Nathaniel Nelson</Link>
             <div className='flex space-x-4'>
-                <Link to="/" className='text-red-500'>Home</Link>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
+                <Link to="/" onClick={()=>{setMenu('home')}} className={menu == 'home' ? "text-red-500" : ""}>Home</Link>
+                <Link to="/resume" onClick={()=>{setMenu('resume')}} className={menu == 'resume' ? "text-red-500" : ""}>Resume</Link>
+                <Link to="/projects" onClick={()=>{setMenu('projects')}} className={menu == 'projects' ? "text-red-500" : ""}>Projects</Link>
             </div>
         </div>
     </nav>
--- src/Views/Resume.js
+++ src/Views/Resume.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>This is the resume</div>
+    <div className='flex flex-col space-y-8 justify-center items-center'>
+        <img src={require("../assets/img/nahtancv.png")} alt="" className='mt-4'/>
+        <button className='bg-red-500 text-white p-2 rounded-lg w-1/5'>
+            Download Cv
+        </button>
+    </div>
   )
 }
 
